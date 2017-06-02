dcc795413c050c171c61ecd5e4c3f6d0dc51cecc
Iuliia Glazeva <zhotter@mail.ru>
2017-06-02 17:46:51 +0200

    created login

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ const sequelize = new Sequelize('blogapp', process.env.POSTGRES_USER,process.env
 	}
 });
 
-sequelize.sync({forse: true})
-
 var User = sequelize.define('user', {
     name: Sequelize.STRING,
     password: Sequelize.STRING
@@ -28,16 +26,63 @@ app.use(session({
 	saveUninitialized: false
 }));
 
-app.use('/', bodyParser);
-app.set('views', 'views');
+//app.use('/', bodyParser);
+app.set('views', 'view');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	response.render('index', {
-		message: req.query.message,
+
+	res.render('index', {
 		user: req.session.user
 	});
+
 });
+
+app.get('/signup', (req, res) => {
+    res.render('signup', {
+        user: session.user
+    });
+});
+
+app.get('/profile', function (request, response) {
+    var user = request.session.user;
+    if (user === undefined) {
+        response.end("please log in");
+    } else {
+        response.render('profile', {
+            user: user
+        });
+    }
+});
+
+app.post('/login', bodyParser.urlencoded({extended: true}), function (request, response) {
+    if(request.body.email.length === 0) {
+        response.end('Please fill out your email address')
+        response.redirect('/login');
+        return;
+    }
+
+    if(request.body.password.length === 0) {
+        response.end('Please fill out your password');
+        response.redirect('/login')
+    }
+
+    User.findOne({
+        where: {
+            email: request.body.email
+        }
+    }).then(function (user) {
+        if (user !== null && request.body.password === user.password) {
+            request.session.user = user;
+            response.redirect('/profile');
+        } else {
+            response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+        }
+    }, function (error) {
+        response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+    });
+});
+
 
 
 sequelize.sync({force: true}).then(function () {
@@ -46,7 +91,7 @@ sequelize.sync({force: true}).then(function () {
         password: "not_password"
     }).then(function () {
         var server = app.listen(3000, function () {
-            console.log('BlogApp: ' + server.address().port);
+            console.log('BlogApp listening on port: ' + server.address().port);
         });
     });
     }, function (error) {
