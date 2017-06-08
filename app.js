a3e86d9f4158308c9637c22c6d4d3d630232ce99
Iuliia Glazeva <zhotter@mail.ru>
2017-06-08 14:42:49 +0200

    created new seite 'messages[C and registr.file'

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
+var Sequelize = require('sequelize');
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-
-
-
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('blogapp', process.env.POSTGRES_USER,process.env.POSTGRES_PASSWORD, {
-	host: 'localhost',
-	dialect: 'postgres',
-	define:{
-		timestamps: false
-	}
+var sequelize = new Sequelize('blogapp', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+    host: 'localhost',
+    dialect: 'postgres',
+    define: {
+        timestamps: false
+    }
 });
 
 var User = sequelize.define('user', {
@@ -20,94 +17,129 @@ var User = sequelize.define('user', {
     password: Sequelize.STRING
 });
 
+var Message = sequelize.define('message', {
+    title: Sequelize.STRING,
+    message: Sequelize.STRING
+})
+
 var app = express();
 
-app.use(session({
-	secret: 'some text',
-	resave:true,
-	saveUninitialized: false
+app.use(bodyParser.urlencoded({
+    extended: false
 }));
 
-//app.use('/', bodyParser);
+app.use(session({
+    secret: 'some text',
+    resave: true,
+    saveUninitialized: false
+}));
+
 app.set('views', 'view');
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-
-	res.render('index', {
-        message: req.query.message,
-		user: req.session.user
-	});
-
+app.get('/', function (request, response) {
+    response.render('index', {
+        message: request.query.message,
+        user: request.session.user
+    });
 });
 
-
-app.get('/profile', (req, res) => {
-    var user = req.session.user;
+app.get('/profile', function (request, response) {
+    var user = request.session.user;
     if (user === undefined) {
-        res.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+        response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
     } else {
-        res.render('profile', {
+        response.render('profile', {
             user: user
         });
     }
 });
 
-app.get('/signup', (req, res) => {
-    res.render('signup', {
-        user: session.user
-    });
-});
-
-app.post('/login', bodyParser.urlencoded({extended: true}), (req, res) => {
-    if(req.body.email.length === 0) {
-        res.redirect('/?message=' + encodeURIComponent("Please fill out your email address."));
+app.post('/login', function (request, response) {
+    if(request.body.email.length === 0) {
+        response.redirect('/?message=' + encodeURIComponent("Please fill out your email address."));
         return;
     }
 
-    if(req.body.password.length === 0) {
-        res.redirect('/?message=' + encodeURIComponent('Please fill out your password.'));
+    if(request.body.password.length === 0) {
+        response.redirect('/?message=' + encodeURIComponent("Please fill out your password."));
         return;
     }
 
     User.findOne({
         where: {
-            email: req.body.email
+            email: request.body.email
         }
-        
     }).then(function (user) {
-        if (user !== null && req.body.password === user.password) {
-            req.session.user = user;
-            res.redirect('/profile');
+        if (user !== null && request.body.password === user.password) {
+            request.session.user = user;
+            response.redirect('/profile');
         } else {
-            res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+            response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
         }
     }, function (error) {
-        res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+        response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
     });
 });
 
-app.get('/logout', (req, res) => {
-    req.session.destroy(function(error){
-        if(error){
+app.get('/logout', function (request, response) {
+    request.session.destroy(function(error) {
+        if(error) {
             throw error;
         }
-        res.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
+        response.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
+    })
+});
+
+app.get('/signup', function (request, response) {
+    response.render('signup', {
     });
 });
+
+app.post('/signup', function(request, response){
+    sequelize.sync({force: true})
+    .then(() => {
+        User.create({
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password
+        })
+        .then(() => {
+            response.redirect('/profile');
+        })
+    })
+})
+
+app.get('/messages', function(request, response) =>{
+    response.render('messages', {
+
+    });
+});
+ app.post('/messages', function(request, response){
+    sequelize.sync({force: true})
+    .then(() => {
+        Message.create({
+        title: request.body.title,
+        body: request.body.body
+        })
+        .then(() => {
+        response.redirect('/newpost')
+        })
+    })
+
+ })   
 
 sequelize.sync({force: true}).then(function () {
     User.create({
         name: "stabbins",
         email: "iuliia@gmail.com",
         password: "not_password"
-
     }).then(function () {
         var server = app.listen(3000, function () {
-            console.log('BlogApp listening on port: ' + server.address().port);
+            console.log('Example app listening on port: ' + server.address().port);
         });
     });
-    }, function (error) {
+}, function (error) {
     console.log('sync failed: ');
     console.log(error);
 });
