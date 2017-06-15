3e85de3b1017b82d6fc47ada54a0e96880319e68
Iuliia Glazeva <zhotter@mail.ru>
2017-06-15 11:06:53 +0200

    created comment file

--- app.js
+++ app.js
@@ -17,15 +17,25 @@ var User = sequelize.define('user', {
     password: Sequelize.STRING
 });
 
-var Message = sequelize.define('message', {
+var Post = sequelize.define('post', {
     title: Sequelize.STRING,
-    message: Sequelize.STRING
+    post: Sequelize.STRING
 })
 
-User.hasMany(Message)
-Message.belongsTo(User)
+var Comment = sequelize.define('comment', {
+    comment: Sequelize.STRING
+})
 
-var app = express();
+User.hasMany(Post)
+Post.belongsTo(User)
+Post.hasMany(Comment)
+User.hasMany(Comment)
+Comment.belongsTo(Post)
+Comment.belongsTo(User)
+
+sequelize.sync({force: false})
+
+const app = express();
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -40,162 +50,251 @@ app.use(session({
 app.set('views', 'view');
 app.set('view engine', 'pug');
 
-app.get('/', function (request, response) {
-    response.render('index', {
-        message: request.query.message,
-        user: request.session.user
+app.get('/', function (req, res) {
+
+    res.render('index', {
+        post: req.query.post,
+        user: req.session.user,
+        
+        
     });
 });
 
-app.get('/profile', function (request, response) {
-    var user = request.session.user;
-    if (user === undefined) {
-        response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
-    } else {
-        response.render('profile', {
-            user: user
-        });
-    }
-});
 
-app.post('/login', function (request, response) {
-    if(request.body.email.length === 0) {
-        response.redirect('/?message=' + encodeURIComponent("Please fill out your email address."));
+app.post('/login', (req, res) => {
+    if(req.body.email.length === 0) {
+        res.redirect('/?message=' + encodeURIComponent("Please fill out your email address."));
         return;
     }
 
-    if(request.body.password.length === 0) {
-        response.redirect('/?message=' + encodeURIComponent("Please fill out your password."));
+    if(req.body.password.length === 0) {
+        res.redirect('/?message=' + encodeURIComponent("Please fill out your password."));
         return;
     }
 
     User.findOne({
         where: {
-            email: request.body.email
+            email: req.body.email
         }
-    }).then(function (user) {
-        if (user !== null && request.body.password === user.password) {
-            request.session.user = user;
-            response.redirect('/profile');
+    })
+    .then(function (user) {
+        //here you cannot directly use the if-statement anymore
+        //you need to use bcrypt.compare for the passwords
+        if (user !== null) {
+            //bcrypt.compare
+            // you cannot use bcrypt.compareSync
+
+            //this goes in the callback
+            req.session.user = user;
+            console.log('logged in with', user)
+            res.redirect('/profile');
+
+
+
+        // old version
+        // if (user !== null && request.body.password === user.password) {
+        //     request.session.user = user;
+        //     console.log('logged in with', user)
+        //     response.redirect('/profile');
+
+
+
         } else {
-            response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+            res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
         }
-    }, function (error) {
-        response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+    }, 
+    function (error) {
+        res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
     });
 });
 
-app.get('/logout', function (request, response) {
-    request.session.destroy(function(error) {
+app.get('/logout', (req, res) => {
+    req.session.destroy(function(error) {
         if(error) {
             throw error;
         }
-        response.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
+
+        res.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
     })
 });
 
-app.get('/signup', function (request, response) {
-    response.render('signup', {
+app.get('/profile', (req, res) => {
+    var user = req.session.user;
+    if (user === undefined) {
+        res.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+    } else {
+        res.render('profile', {
+            user: user
+        });
+    }
+});
+
+
+app.get('/signup', (req, res) => {
+    res.render('signup', {
     });
 });
 
-app.post('/signup', function(request, response){
-    sequelize.sync()
-    .then(() => {
-        User.create({
-        name: request.body.name,
-        email: request.body.email,
-        password: request.body.password
-        })
-        .then(() => {
-            response.redirect('/profile');
-        })
-    })
-})
-
-
-app.get('/messages', function(request, response){
-    Message.findAll()
-        .then((messages)=>{
-            response.render('messages', {
-                messages: messages
-            });
-        })
-});
-
-app.post('/messages', function(request, response){
-    // //welcher user schreibt die Nachricht?
+app.post('/signup', (req, res) => {
+    // check email im DB
         User.findOne({
             where: {
-                id: request.session.user.id //userId des eingelogten Users
+                    email: req.body.email
             }
         })
         .then((user) => {
-            console.log(user);
-            user.createMessage({
-                title: request.body.title,
-                message: request.body.message
-            })
-            .then(() => {
-                response.redirect('/myposts')
-            })
-        });
-    //user.createMessage
-     Message.create({
-        title: request.body.title,
-        message: request.body.message
-    })
-    .then(() => {
-        response.redirect('/myposts')
-    })
+            if(user !== null && req.body.email=== user.email) {
+                res.redirect('/?message=' + encodeURIComponent("Email already exists!"));
+                return;
+            }
+            else{
+                bcrypt.hash(req.body.password, null, null, (err, hash) =>{
+                    if (err) {
+                        throw err
+                    }
+                    User.sync()
+                    .then(() => {
+                        User.create({
+                            name: req.body.name,
+                            email: req.body.email,
+                            password: hash,
+                    
+                        })
+                    })
+                    .then(() =>{
+                        res.redirect('/login')
+                    })
+                    .then().catch(error=> console.log(error))
+                })
+            }
+        })
+        
+    .then().catch(error => console.log(error))
 })
+
+app.get('/post', function(req, res){
+    var user = req.session.user;
+    if (user === undefined) {
+        res.redirect('/?message=' + encodeURIComponent("Please log in to post a message."));
+    } 
+    else {
+        Post.findAll()
+            .then((posts)=>{
+                res.render('post', {
+                posts: posts
+            });
+        })
+    }
+});
+
+app.post('/post', function(req, res){
+    var user = req.session.user;
+        Post.findOne({
+            where: {
+                userId: user.id //userId des eingelogten Users
+            }
+        })
+        .then((post) => {
+            console.log(user);
+            Post.create({
+                title: req.body.title,
+                post: req.body.post,
+                userId: user.id
+            })
+        .then(() => {
+            res.redirect('/myposts')
+        })
+    })
+
+});
 
  
-app.get('/specialPost', function(request, response) {
-    var postId = request.session.id;
-    Message.findOne({
-        where {         
-            id: request.session.user.id
-        }
-          include
-    })
-   
-  
-})
+// app.get('/allposts', function(request, response) {
+//     var user = request.session.user;
+//     if (user === undefined) {
+//         response.redirect('/?message=' + encodeURIComponent("Please log in to post a message."));
+//     } else {
+//     Post.findAll({
+//         where: {         
+//             userId: user.id
+//         },
+//         include: [{
+//                 model: User},
+//                 {
+//                 model: Post},
+//                 {
+//                 model: Comment}]
+//     }) 
+//     .then((posts) => {
+//         response.render('myposts')
+//     })
 
- app.get('/myposts', function(request, response){
-    console.log(request.session.user)
-    Message.findAll({
-        where: {
-            id: request.session.user.id
-        }
-    })
+//     } 
+// })
+
+ app.get('/myposts', (req, res) => {
+    var user = req.session.user;
+    Post.findAll({
+            where: {         
+                userId: user.id
+            },
+            include: [{
+                model: User
+            },
+            {
+                model: Comment,
+                include: [User]
+            }]
+        }) 
     .then( (posts) => {
+        console.log('my amaizing posts')
         console.log(posts)
-        response.render('myposts', {posts: posts})  
+        res.render('myposts', {posts: posts})  
     })
 })
 
-app.get('/allposts', function(request, response){
-    console.log(request.session.user)
-    Message.findAll()
-    .then( (posts) => {
-        response.render('allposts', 
+app.get('/allposts', (req, res) => {
+    var user = req.session.user;
+    if (user === undefined) {
+        res.redirect('/?message=' + encodeURIComponent("Please log in to post a message."));
+    } 
+    else {
+        Post.findAll({
+            include: [{
+                model: User
+            },
+            {
+                model: Comment,
+                include: [User]
+            }]
+        }) 
+        .then( (posts) => {
+        res.render('allposts', 
             {posts: posts})  
-    })
+        })
+    }
 })
 
-sequelize.sync({force: false}).then(function () {
-    User.create({
-        name: "stabbins",
-        email: "iuliia@gmail.com",
-        password: "not_password"
-    }).then(function () {
-        var server = app.listen(3000, function () {
-            console.log('Example app listening on port: ' + server.address().port);
-        });
-    });
-}, function (error) {
-    console.log('sync failed: ');
-    console.log(error);
+app.post('/comments', (req, res) => {
+    const user = req.session.user;
+    console.log('req.query.amazingPostId', req.query.amazingPostId)
+    if (user === undefined) {
+        res.redirect('/?message=' + encodeURIComponent("Please log in."));
+    } 
+    else{
+
+        Comment.create({
+           comment: req.body.comment,
+           postId: req.query.amazingPostId, //linked to post
+           userId: user.id
+        })
+        .then(() => {
+            res.redirect('/myposts')
+        })
+    }
+})
+
+var server = app.listen(3000, function () {
+    console.log('Example app listening on port: ' + server.address().port);
 });
+
